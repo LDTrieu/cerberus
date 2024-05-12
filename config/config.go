3f5ab43d073ff152049f43592b06aa264ee41034
letrieu106 <letrieu106@gmail.com>
2024-05-12 17:40:40 +0700

    remove

--- config/config.go
+++
@@ -1,9 +0,0 @@
-package config
-
-// Server config struct
-type ServerConfig struct {
-	AppVersion  string
-	Port        string
-	Mode        string
-	JwtCertFile string
-}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module mysql_test
+
+go 1.22.2
