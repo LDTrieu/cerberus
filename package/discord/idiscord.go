1343f5c6c95fe14fd3d4cc8aa3e77c199288373b
letrieu106 <letrieu106@gmail.com>
2024-05-12 14:27:17 +0700

    add: gitignore

--- package/discord/idiscord.go
+++
@@ -1,6 +0,0 @@
-package discord
-
-type IDiscordNotification interface {
-	SendError(message string, fields ...EmbedFields) error
-	SendNotification(color string, message string, fields ...EmbedFields) error
-}
